2f35560593f83e54798a6145a89665c9bcbd1f82
vivaca86 <vivaca86@gmail.com>
2026-04-14 12:12:46 +0900

    Force dashboard to load from sheet in production

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.PP_CONFIG = {
-  gatewayUrl: "https://script.google.com/macros/s/AKfycbwTTTBD3kVxTvxOFIqIi-TsnZsZr-ugZNylfSCXChGunqO-q-zQmBaTU_YCCqkZf_dvTw/exec"
+  gatewayUrl: "https://script.google.com/macros/s/AKfycbwTTTBD3kVxTvxOFIqIi-TsnZsZr-ugZNylfSCXChGunqO-q-zQmBaTU_YCCqkZf_dvTw/exec",
+  dashboardSourceMode: "sheet"
 };
